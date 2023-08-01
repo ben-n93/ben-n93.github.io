4414871022f37adc6c9814220343689090f12c60
Benjamin Nour <ben.nour@live.com.au>
2023-08-01 20:39:11 +1000

    updated seo configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ AUTHOR_FEED_RSS = None
 
 # SEO
 SEO_REPORT = True  # SEO report is enabled by default
-SEO_ENHANCER = False  # SEO enhancer is disabled by default
+SEO_ENHANCER = True  # SEO enhancer is disabled by default
 SEO_ENHANCER_OPEN_GRAPH = False # Subfeature of SEO enhancer
 SEO_ENHANCER_TWITTER_CARDS = False # Subfeature of SEO enhancer
 
